b36b15bdc5348c319880f0dca7dbb2245e2fb735
Capa2 <johan931@gmail.com>
2024-11-06 14:33:46 +0100

    refactor

--- src/excel_writer.py
+++
@@ -1,5 +0,0 @@
-#excel_writer.py
-def write_from_iterator(iterator, path):
-    with open(path, 'wb') as file:
-        for chunk in iterator:
-            file.write(chunk)
--- src/main.py
+++ src/main.py
@@ -1,29 +1,31 @@
 # main.py
-import config_loader as config_loader
-from tabulate import tabulate
-import request_handler
+from lib.core import report_fetcher, report_processor
+from lib.services import async_download_service
+from lib.utils import config_manager
+from lib.utils.logging_setup import detailed_logger, summary_logger
 import asyncio
-import downloader
 
-config = config_loader.load_config()
+settings = config_manager.load_settings()
 
 async def main():
-    print("Starting main...")
+    summary_logger.info("Starting main process...")
 
-    requests = request_handler.get_requests()
+    try:
+        reports = report_fetcher.fetch_reports()
+        detailed_logger.info(f"Retrieved {len(reports)} links to reports.")
 
-    print(f"Retrieved {len(requests)} requests")
+        queue = asyncio.Queue()
+        validation_complete_event = asyncio.Event()
 
-    queue = asyncio.Queue()
+        await asyncio.gather(
+            report_processor.process_reports(reports, queue, validation_complete_event),
+            async_download_service.download_from_queue(queue, validation_complete_event)
+        )
 
-    validation_complete_event = asyncio.Event()
-
-    await asyncio.gather(
-        request_handler.validate_requests(requests, queue, validation_complete_event),
-        downloader.download_from_queue(queue, validation_complete_event)
-    )
-
-    print("Main completed")
+        summary_logger.info("Main process completed successfully.")
+    except Exception as e:
+        detailed_logger.error(f"An error occurred in the main process: {e}")
+        summary_logger.error("Main process failed due to an error.")
 
 if __name__ == "__main__":
     asyncio.run(main())
